71acaef321a0b33f90999556b84a751a56dc3e68
Bojan Mihelac <bmihelac@mihelac.org>
2013-03-15 18:48:42 +0100

    Add advanced example

--- tests/core/admin.py
+++ tests/core/admin.py
@@ -6,6 +6,9 @@ from .models import (
         ObjectAttribute,
         Attribute,
         Tshirt,
+        AdvancedAttribute,
+        AdvancedObjectAttribute,
+        Book
         )
 
 
@@ -13,5 +16,21 @@ class TshirtAdmin(AttributeEditMixin, admin.ModelAdmin):
     attribute_models = [ObjectAttribute]
 
 
+class BookAdmin(AttributeEditMixin, admin.ModelAdmin):
+    attribute_models = [ObjectAttribute, AdvancedObjectAttribute]
+
+    def get_objattributes_form_class(self, object_attribute):
+        if isinstance(object_attribute, AdvancedObjectAttribute):
+            from django.forms.models import modelform_factory
+            from objattributes.forms import ObjectAttributeModelForm
+            fields = object_attribute.get_editable_fields()
+            return modelform_factory(type(object_attribute),
+                    form=ObjectAttributeModelForm,
+                    fields=fields)
+        return super(BookAdmin, self).get_objattributes_form_class(
+                object_attribute_model, attribute)
+
 admin.site.register(Attribute)
 admin.site.register(Tshirt, TshirtAdmin)
+admin.site.register(AdvancedAttribute)
+admin.site.register(Book, BookAdmin)
--- tests/core/models.py
+++ tests/core/models.py
@@ -28,3 +28,57 @@ class ObjectAttribute(BaseObjectAttribute):
 class Tshirt(models.Model):
     name = models.CharField(max_length=100)
     attributes = GenericRelation(ObjectAttribute)
+
+
+ATTRIBUTE_CHOICES = (
+        ('string', 'String'),
+        ('number', 'Integer'),
+        ('boolean', 'Boolean'),
+        )
+
+
+class AdvancedAttribute(BaseAttribute):
+    name = models.CharField(_('Name'), max_length=250)
+    description = models.CharField(_('Description'), max_length=100, blank=True)
+    type = models.CharField(_('Type'), choices=ATTRIBUTE_CHOICES, max_length=30)
+    ordering = models.IntegerField(_('Ordering'), default=0)
+
+    class Meta:
+        verbose_name = _('AdvancedAttribute')
+        verbose_name_plural = _('AdvancedAttributes')
+        ordering = []
+
+    def __unicode__(self):
+        return self.name
+
+
+class AdvancedObjectAttribute(BaseObjectAttribute):
+    attribute = models.ForeignKey(AdvancedAttribute,
+            verbose_name=_('Attribute'),
+            related_name="object_attributes")
+    string_value = models.CharField(_('String'), max_length=250, blank=True,
+            null=True)
+    number_value = models.DecimalField(_('Number'), decimal_places=4,
+            max_digits=10, blank=True, null=True)
+    boolean_value = models.NullBooleanField('Boolean value', blank=True,
+            null=True)
+
+    def get_field_name_for_attribute(self):
+        return "%s_value" % self.attribute.type
+
+    def get_editable_fields(self):
+        return [self.get_field_name_for_attribute()]
+
+    def set_value(self, value):
+        setattr(self, self.get_field_name_for_attribute(), value)
+
+    def get_value(self):
+        getattr(self, self.get_field_name_for_attribute(), value)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    attributes = GenericRelation(AdvancedObjectAttribute)
+
+    def __unicode__(self):
+        return self.name
